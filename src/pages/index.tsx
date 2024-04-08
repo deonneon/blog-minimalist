9ef6ea1087c266707ad4630528d62400f823be7f
deonneon <dtnifti@gmail.com>
2024-04-07 21:27:51 -0500

    add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+import { useGesture } from "react-use-gesture";
 import Layout from "../components/Layout";
 import BasicMeta from "../components/meta/BasicMeta";
 import OpenGraphMeta from "../components/meta/OpenGraphMeta";
@@ -5,14 +7,30 @@ import TwitterCardMeta from "../components/meta/TwitterCardMeta";
 import { SocialList } from "../components/SocialList";
 
 export default function Index() {
+  const originalText = "A haven for entropic ideas.";
+  const [h2Text, setH2Text] = useState(originalText);
+
+  const bind = useGesture({
+    onDrag: ({ direction, velocity }) => {
+      // Check for an upward swipe
+      if (direction[1] < 0 && velocity > 1) {
+        setH2Text("A storyline for procrastinators.");
+      }
+      // Check for a downward swipe
+      else if (direction[1] > 0 && velocity > 1) {
+        setH2Text(originalText);
+      }
+    },
+  });
+
   return (
     <Layout>
       <BasicMeta url={"/"} />
       <OpenGraphMeta url={"/"} />
       <TwitterCardMeta url={"/"} />
-      <div className="container">
+      <div className="container" {...bind()}>
         <div>
-          <h2>A haven for entropic ideas.</h2>
+          <h2>{h2Text}</h2>
           <SocialList />
         </div>
       </div>
